f429d67da108afd2656131453ff61d7bf8ee067f
kinalmangukiya <manguk01@email.franklin.edu>
2025-01-07 20:58:08 -0600

    Changed HTML files And added SQL

--- src/main/java/com/example/demo/Controller/HomepageController.java
+++ src/main/java/com/example/demo/Controller/HomepageController.java
@@ -3,6 +3,7 @@ package com.example.demo.Controller;
 
 import com.example.demo.Model.Customer;
 import com.example.demo.Model.User;
+import com.example.demo.Repository.CustomerRepository;
 import com.example.demo.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.sql.Date;
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -20,6 +22,9 @@ public class HomepageController {
 
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    CustomerRepository customerRepository;
 
 
     @GetMapping("/login")
@@ -68,4 +73,78 @@ public class HomepageController {
         return "redirect:/login";
     }
 
+    @GetMapping("/customersection/search")
+    public String searchpage(){
+
+        return "customerpages/customersearch";
+    }
+
+    @PostMapping("/customersection/search")
+    public String searchpage(@RequestParam String keyword, String type, Model model){
+
+        List<Customer> customerlist = customerRepository.findAll();
+
+        ArrayList<Customer> resultlist = new ArrayList<>();
+        switch(type){
+            case "lname":
+
+                for(Customer s : customerlist){
+                    if(s.getLname().toLowerCase().contains(keyword.toLowerCase())){
+                        resultlist.add(s);
+                    }
+                }
+                break;
+            case "fname":
+
+                for(Customer s : customerlist){
+                    if(s.getFname().toLowerCase().contains(keyword.toLowerCase())){
+                        resultlist.add(s);
+                    }
+                }
+                break;
+
+            case "city" :
+            for(Customer s : customerlist){
+                if(s.getCity().toLowerCase().contains(keyword.toLowerCase())){
+                    resultlist.add(s);
+                }
+            }
+            break;
+
+            case "state" :
+                for(Customer s : customerlist){
+                    if(s.getState().toLowerCase().contains(keyword.toLowerCase())){
+                        resultlist.add(s);
+                    }
+                }
+
+                break;
+        }
+        model.addAttribute("customerlist",resultlist);
+        return "customerpages/searchresults";
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/example/demo/Repository/CustomerRepository.java
+++ src/main/java/com/example/demo/Repository/CustomerRepository.java
@@ -1,9 +1,20 @@
 package com.example.demo.Repository;
 
 import com.example.demo.Model.Customer;
+import com.example.demo.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+
+    @Query(value = "SELECT * FROM user WHERE username = :name", nativeQuery = true)
+    User findUserByName(@Param("name") String name);
+
+
 }
